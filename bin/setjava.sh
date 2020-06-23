13ad2e791cf24cfd7bb2e2e6f870b6c32cf8bb6d
Jason A Everling <jason.everling@gmail.com>
2020-06-22 19:13:20 -0500

    refactor setjava.sh

--- bin/setjava.sh
+++ bin/setjava.sh
@@ -8,7 +8,7 @@ if [ "$JAVA_VERSION" == 8 ];then
     tar xzf /opt/java-8-openjdk.tgz -C /opt && \
     mv /opt/amazon-corretto-* /opt/java-8-openjdk && \
     rm /opt/java-8-openjdk.tgz
-else 
+elif [ "$JAVA_VERSION" == 11 ];then 
     wget \
     --quiet \
     --no-cookies \
@@ -16,5 +16,11 @@ else
     tar xzf /opt/java-11-openjdk.tgz -C /opt && \
     mv /opt/amazon-corretto-* /opt/java-11-openjdk && \
     rm /opt/java-11-openjdk.tgz
+elif [ -z "$JAVA_VERSION" ];then 
+    echo "Java version not specified"
+    exit 1
+else
+    echo "Java version not supported, it must be one of 8 or 11, you entered:" $JAVA_VERSION
+    exit 1
 fi
 echo "Installed OpenJDK Version:" $JAVA_VERSION
